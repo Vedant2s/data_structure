2b013d6e9ca5908a0567d7e47849e7deabb03b4d
Vedant2s <98942578+Vedant2s@users.noreply.github.com>
2022-12-13 07:24:01 +0530

    Update circularqueuearray.c

--- circularqueuearray.c
+++ circularqueuearray.c
@@ -1,103 +1,110 @@
 #include<stdio.h>
 #define max 5
-
 int q[max]={0};
 int front=-1,rear=-1;
-
 void enqueue()
 {
-    int x;
-    if((front==0&&rear==max-1)||(rear+1==front))
-    {
-        printf("OVERFLOW\n");
-        return;
-    }
-    else
-    {
-        printf("Enter the element\n");
-        scanf("%d",&x);
-        if(rear==-1)
-        {
-            front=0;
-            rear=0;
-        }
-        else if(rear==max-1)
-        {
-            rear=0;
-        }
-        else
-        {
-            rear++;
-        }
-        q[rear]=x;
-    }
+ int x;
+ if((front==0&&rear==max-1)||(rear+1==front))
+ {
+ printf("OVERFLOW\n");
+ return;
+ }
+ else
+ {
+ printf("Enter the element\n");
+ scanf("%d",&x);
+ if(rear==-1)
+ {
+ front=0;
+ rear=0;
+ }
+ else if(rear==max-1)
+ {
+ rear=0;
+ }
+ else
+ {
+ rear++;
+ }
+ q[rear]=x;
+ }
 }
 void dequeue()
 {
-    int a;
-    if(front==-1)
-    {
-        printf("UNDERFLOW\n");
-        return;
-    }
-    else
-    {
-        a=q[front];
-        if(front==rear)
-        {
-            front=-1;
-            rear=-1;
-        }
-        else if(front==max-1)
-        {
-            front=0;
-        }
-        else
-        {
-            front++;
-        }
-        printf("Deleted element is %d\n",a);
-    }
+ int a;
+ if(front==-1)
+ {
+ printf("UNDERFLOW\n");
+ return;
+ }
+ else
+ {
+ a=q[front];
+ if(front==rear)
+ {
+ front=-1;
+ rear=-1;
+ }
+ else if(front==max-1)
+ {
+ front=0;
+ }
+ else
+ {
+ front++;
+ }
+ printf("Deleted element is %d\n",a);
+ }
 }
 void display()
 {
-    int i;
-    if(front==-1&&rear==-1)
-    {
-        printf("EMPTY/UNDERFLOW\n");
-        return;
-    }
-    if(front>rear)
-    {
-        printf("The elements in circular queue are:\n");
-        for(i=front;i<max;i++)
-        {
-            printf("%d\n",q[i]);
-        }
-        for(i=0;i<=rear;i++)
-        {
-            printf("%d\n",q[i]);
-        }
-    }
-    else
-    {
-        printf("The elements in circular queue are:\n");
-        for(i=front;i<max;i++)
-        {
-            printf("%d\n",q[i]);
-        }
-    }
+ int i;
+ if(front==-1&&rear==-1)
+ {
+ printf("EMPTY/UNDERFLOW\n");
+ return;
+ }
+ if(front>rear)
+ {
+ printf("The elements in circular queue are:\n");
+ for(i=front;i<max;i++)
+ {
+ printf("%d\n",q[i]);
+ }
+ for(i=0;i<=rear;i++)
+ {
+ printf("%d\n",q[i]);
+ }
+ }
+ else
+ {
+ printf("The elements in circular queue are:\n");
+ for(i=front;i<=rear;i++)
+ {
+ printf("%d\n",q[i]);
+ }
+ }
 }
 int main()
 {
-    int choice=0;
-    enqueue();
-    enqueue();
-    enqueue();
-    enqueue();
-    enqueue();
-    enqueue();
-    dequeue();
-    display();
-    return 0;
+ int choice=0;
+ do{
+ printf("1.enter element\n2.delete element\n3.display element\n4.exit\n");
+ scanf("%d",&choice);
+ switch(choice)
+ {
+ case 1:
+ enqueue();
+ break;
+ case 2:
+ dequeue();
+ break;
+ case 3:
+ display();
+ break;
+ }
+ }while(choice!=4);
+ 
+ return 0;
 }
