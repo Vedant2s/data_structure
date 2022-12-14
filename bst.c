1e86606eafd8137678bb6f5fe9e8f6ace67c1589
Vedant2s <98942578+Vedant2s@users.noreply.github.com>
2022-12-14 09:57:05 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -31,7 +31,7 @@ void inorder(struct node* root)
 }
  void search(struct node*root,int key2)
  {
-     if(root!=NULL)
+     if(root==NULL)
      {
          return;
      }
@@ -59,7 +59,19 @@ void preorder(struct node* root)
         inorder(root->right);
     
 }
-
+void postorder(struct node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    else if (root != NULL) {
+		inorder(root->left);
+        
+        }
+        inorder(root->right);
+    printf("%d \n", root->key);
+}
 struct node* insert(struct node* node, int key)
 {
  
@@ -75,7 +87,57 @@ struct node* insert(struct node* node, int key)
 
     return node;
 }
+ struct node* minValueNode(struct node* node)
+{
+    struct node* current = node;
  
+   
+    while (current && current->left != NULL)
+        current = current->left;
+ 
+    return current;
+}
+ 
+
+struct node* deleteNode(struct node* root, int key)
+{
+    
+    if (root == NULL)
+        return root;
+ 
+  
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+ 
+    
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+ 
+   
+    else {
+        
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        }
+ 
+        
+        struct node* temp = minValueNode(root->right);
+ 
+        
+        root->key = temp->key;
+ 
+        
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
 
 int main()
 {
@@ -85,7 +147,7 @@ int main()
 
     int ch,y,d;
     do{
-        printf("\n1.insert\n2.traverse\n");
+        printf("\n1.insert\n2.traverse\n3.search\n4.delete element\n5.preorder\n6.postorder\n");
         scanf("%d",&ch);
         switch(ch)
         {
@@ -94,8 +156,7 @@ int main()
             scanf("%d",&d);
             root=insert(root,d);
             break;
-            case 2:
-            
+            case 2:       
             inorder(root);
             break;
             case 3:
@@ -103,6 +164,17 @@ int main()
             scanf("%d",&d);
             search(root,d);
             break;
+				case 4:
+				printf("enter the data\n");
+            scanf("%d",&d);
+            root=deleteNode(root,d);
+				break;
+				case 5:
+				preorder(root);
+				break;
+				case 6:
+				postorder(root);
+				break;
         }
         printf("do you want to continue?[0/1]\n");
         scanf("%d",&y);
